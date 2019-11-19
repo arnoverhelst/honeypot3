df8bda15762d48937fdc613ba661b09eac8fc600
Arno Verhelst <arno.verhelst@student.howest.be>
2019-11-19 23:44:13 +0100

    comment validation max 50chars & overflow auto for comments

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,6 +1,8 @@
-@foreach($comments as $comment)
-<div class="display-block">
-    <strong href>{{ $comment->user->username }}</strong>
-    <p>{{ $comment->content }}</p>
+<div class="overflow-auto" style="height: 40vh;">
+    @foreach($comments as $comment)
+    <div class="overflow-auto">
+        <strong href>{{ $comment->user->username }}</strong>
+        <p>{{ $comment->content }}</p>
+    </div>
+    @endforeach
 </div>
-@endforeach
